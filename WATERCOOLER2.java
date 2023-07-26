558a0505097fb00b2a18fc22d63bc9e0e84c8e5f
SATENDRA KUMAR <103168059+Stndr1506@users.noreply.github.com>
2023-07-26 12:56:08 +0530

    Add files via upload

---
+++ WATERCOOLER2.java
@@ -0,0 +1,34 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int X=sc.nextInt();
+		    int Y=sc.nextInt();
+		    int a = Y/X;
+		    int b = Y%X;
+		    if(a>1){
+		        if(b!=0){
+		            System.out.println(a);
+		        }
+		        else if(b==0){
+		            System.out.println(a-1);
+		        }
+		    }else if(a==1 && Y>X){
+		        System.out.println(1);
+		    }else{
+		        System.out.println(0);
+		    }
+		}
+	}
+}
